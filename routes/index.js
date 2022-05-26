fbae2cf22dea0c7ca6feac1c97762ea49caf138b
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-05-26 15:46:26 +0800

    feat: add logout

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
 router.get('/index', userController.getIndex)
 
