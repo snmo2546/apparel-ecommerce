58b9a3499a8620ab0a095c3fc146602ccfa3f65a
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-05-26 16:35:38 +0800

    feat: add general error handler

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const passport = require('../config/passport')
 const userController = require('../controller/user-controller')
 const admin = require('./modules/admin')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
@@ -17,5 +19,6 @@ router.get('/logout', userController.logout)
 router.get('/index', userController.getIndex)
 
 router.use('/', (req, res) => res.redirect('/index'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
