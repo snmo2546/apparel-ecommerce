c44b156085d2c0928287dbeb68b35cd9aeb1d632
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-06-02 15:44:27 +0800

    feat: use isAuthenticated to display different navbars

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.isAuthenticated = helpers.ensureAuthenticated(req)
   res.locals.user = helpers.getUser(req)
   next()
 })
