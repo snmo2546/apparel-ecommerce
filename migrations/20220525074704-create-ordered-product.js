97dd5df07538abca9e255ab0e45c7394d2e1ca89
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-06-07 12:00:53 +0800

    feat: modify ordered_products table name

--- migrations/20220525074704-create-ordered-product.js
+++ migrations/20220525074704-create-ordered-product.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('OrderedProducts', {
+    await queryInterface.createTable('Ordered_Products', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('OrderedProducts')
+    await queryInterface.dropTable('Ordered_Products')
   }
 }
