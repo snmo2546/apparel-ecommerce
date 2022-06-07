a6eb967f15f73e17270401bb6f5fd8c2373d39f4
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-06-07 12:08:53 +0800

    feat: add shipment method model

---
+++ models/shipmentmethod.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ShipmentMethod extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ShipmentMethod.init({
+    name: DataTypes.STRING,
+    fee: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ShipmentMethod',
+    tableName: 'Shipment_Methods',
+    underscored: true,
+  });
+  return ShipmentMethod;
+};
