9a8e40afa980afc2a16ced770d69261450c8281f
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-06-21 16:38:16 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const bcrypt = require('bcryptjs')
 
 const { User, Cart, Product } = require('../models')
@@ -43,6 +44,33 @@ passport.use(new FacebookStrategy(
           .then(salt => bcrypt.hash(randomPassword, salt))
           .then(hash => User.create({
             name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }
+))
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    const { email } = profile._json
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name: profile.displayName,
             email,
             password: hash
           }))
